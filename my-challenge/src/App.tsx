04ebc953a30f769ef7febbc2fe18ab61d23d5abc
f.farrokhi <sarafarokhi.ozon@gmail.com>
2025-03-30 20:14:34 +0330

    chore: (App) import components

--- my-challenge/src/App.tsx
+++ my-challenge/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import { Container, Typography, Paper } from "@mui/material";
+import AddTodoForm from "./components/todo/AddTodoForm";
+import TodoList from "./components/todo/TodoList";
 
 function App() {
-  return <>hi</>;
+  return (
+    <Container maxWidth="sm">
+      <Paper elevation={3} style={{ padding: "20px", marginTop: "20px" }}>
+        <Typography variant="h4" align="center" gutterBottom>
+          لیست کارها
+        </Typography>
+        <AddTodoForm />
+        <TodoList />
+      </Paper>
+    </Container>
+  );
 }
 
 export default App;
